b43a60b972ece88dd4e0a5784be37bbacab442b6
LoganMartinez <lmartin9@trinity.edu>
2022-06-17 16:47:30 -0500

    changed create_elsa_data.sh to be compatible with HPC

--- Data_Parser/create_elsa_data.sh
+++ Data_Parser/create_elsa_data.sh
@@ -1,5 +1,5 @@
-python3 data_parser_med.py
-python3 data_parser_adl_iadl.py
-python3 data_parser.py
-python3 split_data.py
-python3 data_info.py
+python3 /data/zhanglab/lmartin9/AgingHealth/DataParser/data_parser_med.py
+python3 /data/zhanglab/lmartin9/AgingHealth/DataParser/data_parser_adl_iadl.py
+python3 /data/zhanglab/lmartin9/AgingHealth/DataParser/data_parser.py
+python3 /data/zhanglab/lmartin9/AgingHealth/DataParser/split_data.py
+python3 /data/zhanglab/lmartin9/AgingHealth/DataParser/data_info.py
